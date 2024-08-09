99ca45b28689288f17ebf4f0f1ddcff1a3977c84
tarunAllmysons <tarun@in.allmysons.com>
2024-08-10 01:08:33 +0530

    fast pizza

---
+++ fast-react-pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,67 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+  // cart: [
+  //   {
+  //     pizzaId: 1,
+  //     name: 'Mediterranean',
+  //     quantity: 2,
+  //     unitPrice: 16,
+  //     totalPrice: 32,
+  //   },
+  // ],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      // payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      //payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      //payload = pizzaId;
+      const item = state.card.find((item) => item.pizzaId === action.payload);
+
+      item.quantity++;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    descreaseItemQuantity(state, action) {
+      //payload = pizzaId;
+      const item = state.card.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    clearCart(state, action) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  descreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
